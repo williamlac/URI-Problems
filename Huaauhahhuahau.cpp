7c2a3c491ca9c1aa9f5c5c13b505b7ea73b11e00
WilliamLac <William06>
2017-05-07 18:06:03 -0300

    Huaauhahhuahau.cpp added

--- Huaauhahhuahau.cpp
+++ Huaauhahhuahau.cpp
@@ -2,27 +2,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-	int i, j = 0;
 	string s1, s2, s3;
 	cin>>s1;
-	for(i = 0; i < s1.size() ; i++) {
-		switch(s1[i]) {
-			case 'a': case 'e': case 'i': case 'o': case 'u':
+	for(int i = 0; i < s1.size() ; i++) {
+		if (s[i] == 'a' ||s[i] == 'e' ||s[i] == 'i' ||s[i] == 'o' ||s[i] == 'u')
 				s2+= s1[i];
-				j++;
-				break;
-		}
-		//printf("For  %c   v->  %s\n", risada[i], vogais1 );
 	}
-	s2[j] = '\0';
 	s3 = s2;
 	reverse(s3.begin(), s3.end());
-	if(s2.compare(s3) == 0) {
-		printf("S\n");
-	}
-	else {
-		printf("N\n");
-	}
-
+	if(s2.compare(s3) == 0)
+		cout >> 'S';
+	else
+		cout >> 'N';
 	return 0;
 }
